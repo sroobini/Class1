bbda973f569a72cc3319e8b4dee9fd80c2027235
Ruby <sroobini@users.noreply.github.com>
2017-02-10 05:32:45 -0800

    BrowserStack.java- Roobini
    BrowserStack.java
    y.ho
    f.bk
    y.be
    g.co
    y.ho
    f.bk
    y.be
    g.co
    y.be
    Stack is empty
    DONE

--- BrowserStack.java
+++ BrowserStack.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 
 /**
  * Created by IceBreakers on 2/5/17.
@@ -13,18 +15,24 @@
  *         printStack() - prints the Stack preserving all its values
  *         size()       - returns the length of the stack
  *         isEmpty()    - return true if stack is not empty else false
- *
+ *o--
  */
 public class BrowserStack {
 
     String[] myStack;
     int top;
-    int STACK_SIZE = 16; //INITIAL STACK SIZE, GROW IF ITS NEEDED
+    int STACK_SIZE = 2; //INITIAL STACK SIZE, GROW IF ITS NEEDED
 
     //Constructor to initialize your stack
     BrowserStack(){
         this.myStack = new String[STACK_SIZE];
         this.top = -1;
+    }
+    private void createNewStack(){
+    	String[] newStack = new String[STACK_SIZE * 2];
+    	newStack = Arrays.copyOf(myStack, newStack.length);
+    	STACK_SIZE = STACK_SIZE *2;
+    	myStack = newStack;
     }
 
 
@@ -37,33 +45,96 @@ public class BrowserStack {
     //push() - pushes the element in this case its a url (String)
     private  void push(String url){
         //WRITE YOUR CODE HERE
+    	if(isFull()){
+    		createNewStack();
+    	}
+    	top = top + 1;
+    	myStack[top] = url;
     }
     //pop()- pops the element from the stack
     private String pop(){
         //WRITE YOUR CODE HERE
-
-    }
+    	String popped;
+    	if(top<0){
+    		popped = "Stack is empty";
+    	}
+    	else{
+    	popped = myStack[top];
+    	myStack[top] = null;
+    	}
+    	top--;
+    	return popped;
+    } 
 
     private void printStack(){
         //WRITE YOUR CODE HERE
-
-    }
+    	if(isEmpty()){
+    		System.out.println("Stack is empty");
+    	}
+    	int temptop = -1;
+    	String[] tempStack = new String[myStack.length];
+    	while(top>=0){
+    		peek();
+    		temptop++;
+    		tempStack[temptop] = pop();
+    	}
+    	// Moving the elements to mystack from tempstack
+    	while(temptop>=0){
+    		push(tempStack[temptop]);
+    		temptop--;
+    	}    	
+    	
+    } 
     private  String peek(){
         //WRITE YOUR CODE HERE
+         String peeked;
+    	peeked = myStack[top];
+    	System.out.println(peeked);
+    	return peeked;
+
 
     }
     private  int size(){
         //WRITE YOUR CODE HERE
+        return top+1;
 
     }
     private  boolean isEmpty(){
         //WRITE YOUR CODE HERE
-
+    	if(top==-1){
+    		return true;
+    	}
+    	return false;
+    }
+    private  boolean isFull(){
+        //WRITE YOUR CODE HERE
+    	if(top==(STACK_SIZE-1)){
+    		return true;
+    	}
+    	return false;
     }
     private  boolean search(String url){
         //WRITE YOUR CODE HERE
-
-    }
+    	int temptop = -1;
+    	boolean ispresent =false;
+    	String[] tempStack = new String[myStack.length];
+    	while(top>=0){
+    		if(myStack[top].equalsIgnoreCase(url)){
+    			ispresent =  true;
+    			break;
+    		}
+    		else{
+    			temptop++;
+    			tempStack[temptop] = pop();
+    			}
+    	}
+    	// Moving the elements to mystack from tempstack
+    	while(temptop>=0){
+    		push(tempStack[temptop]);
+    		temptop--;
+    	}
+        return ispresent;
+    } 
     private static void testBench(BrowserStack browserStack){
         //CANNOT CHANGE CODE BELOW. MUST USE AS IS
         browserStack.push("g.co");
@@ -74,7 +145,7 @@ public class BrowserStack {
         myassert(browserStack.search("y.be"));
         myassert(!browserStack.search("y.be1"));
         browserStack.printStack();
-        myassert(browserStack.size() == 4);
+        myassert(browserStack.size() == 4); 
         browserStack.pop();
         myassert(browserStack.pop().equals("f.bk"));
         browserStack.peek();
