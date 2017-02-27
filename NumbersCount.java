ac3b96fcfa749edb77a4c4e575a8f912781097b7
balamuralim <balamurali.manickavasagam@gmail.com>
2017-02-27 10:09:09 -0800

    Number count - assignment

---
+++ NumbersCount.java
@@ -0,0 +1,75 @@
+/*
+ * Write a logic to count of number of numbers in the given input string
+ * Input is String
+ * Output will print the number of occurences for each number
+ * 
+ * Ex. 933262154439441526816992388562667004907159682643816
+ * Output will be 
+ *  0 occurs 3 times
+    1 occurs 5 times
+    2 occurs 6 times
+    3 occurs 5 times
+    4 occurs 6 times
+    5 occurs 4 times
+    6 occurs 9 times
+    7 occurs 2 times
+    8 occurs 5 times
+    9 occurs 6 times
+ * 
+ * Also print the output as bar graph with x axis as numbers
+ * and y axis with "#"  as of number of occurences
+ *  
+ *  
+ *               # 
+ *               #
+ *               #
+ *       #   #   #     # 
+ *     # # # #   #   # #
+ *     # # # # # #   # #
+ *   # # # # # # #   # #
+ *   # # # # # # # # # #
+ *   # # # # # # # # # #
+ *   0 1 2 3 4 5 6 7 8 9 
+ *   
+ *   ex.2 
+ *   
+ *   Input: 2146859296389521599993229915608941463961565182
+ *   
+ *   Ouput: 
+ *  0 occurs 1 times 
+    1 occurs 6 times 
+    2 occurs 6 times
+    3 occurs 3 times
+    4 occurs 3 times
+    5 occurs 6 times
+    6 occurs 6 times
+    7 occurs 0 times
+    8 occurs 4 times
+    9 occurs 11 times
+
+                       
+                       #
+                       #
+ *                     #
+ *                     # 
+ *                     # 
+ *     # #     # #     # 
+ *     # #     # #     #
+ *     # #     # #   # #
+ *     # # # # # #   # #
+ *     # # # # # #   # #
+ *   # # # # # # #   # #
+ *   0 1 2 3 4 5 6 7 8 9 
+
+ *
+ */
+
+public class NumbersCount {
+    public NumbersCount() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        NumbersCount numbersCount = new NumbersCount();
+    }
+}
