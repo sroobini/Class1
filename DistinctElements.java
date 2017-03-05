9bf70917c9b90b1c29d7ad1bdd21cbf5ed0c61d4
chandramouly <chandru.mouly@gmail.com>
2017-03-05 12:50:10 -0800

    Add files via upload

---
+++ DistinctElements.java
@@ -0,0 +1,23 @@
+
+/**
+ * Created by IceBreakers on 3/5/17.
+ * if an input array has distinct elements, return TRUE, otherwise false
+ *  input = [3,4,6,2,1,6,8] - False
+ *  input = [2,3,1,7,6] - True
+ */
+public class DistinctElements {
+    private static boolean isDistinct(int[] nums){
+
+    }
+
+    public static void testBench(){
+        //null case
+        // 1 element array
+        // array with distinct elements case
+        // array with not distinct elements case
+    }
+    public static void main(String args[]){
+        testBench();
+    }
+
+}
---
+++ StringCompressor.java
@@ -0,0 +1,27 @@
+
+/**
+ * Created by Icebreakers on 3/5/17.
+ * Implement a method to perform basic string compression using the counts
+ of repeated characters.
+ For example, the string aabcccccaaa would become a2blc5a3.
+ If the "compressed" string would not become smaller than the original
+ string, your method should return the original string.
+ *
+ */
+public class StringCompressor {
+
+    private static String compressedString(String s){
+
+    }
+
+    public static void testBench(){
+        //null case
+        // 1 character string
+        // string with multiple characters case
+        // compressed string > original string case
+    }
+    public static void main(String args[]){
+        testBench();
+    }
+
+}
